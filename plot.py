a3a31e9dc81b3ba7531185aa3ed4e03cfa7aa6c7
Francesco Russo <franci.russo@icloud.com>
2024-06-04 18:40:39 +0200

    plot confronto mappers

--- plot.py
+++ plot.py
@@ -2,38 +2,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-# I tuoi dati
-data = {
-    'Numero di punti': [500000, 500000, 1000000, 1000000, 5000000, 5000000, 10000000, 10000000],
-    'Tempo': [38.69880962371826, 11.270809173583984, 101.11570715904236, 30.59341073036194, 476.03889989852905, 115.75412392616272, 1498.7870407104492, 300.7421875],
-    'Modalità': ['Distribuito', 'Sequenziale', 'Distribuito', 'Sequenziale', 'Distribuito', 'Sequenziale', 'Distribuito', 'Sequenziale']
-}
+data = pd.read_csv("tempi.csv")
 
 df = pd.DataFrame(data)
 
 # Separa i dati in due DataFrames in base alla modalità
-df_distr = df[df['Modalità'] == 'Distribuito']
-df_seq = df[df['Modalità'] == 'Sequenziale']
-print (df_distr)
+df_15 = df[df["Map"] == 15]
+df_30 = df[(df["Map"] == 30) & (df["Numero di cluster"] == 3)]
+df_60 = df[(df["Map"] == 60)]
+
 
 labels = ['500000', '1000000', '5000000', '10000000']
-distr = [df_distr["Tempo"][0],df_distr["Tempo"][2], df_distr["Tempo"][4], df_distr["Tempo"][6]]
-seq =  [df_seq["Tempo"][1],df_seq["Tempo"][3], df_seq["Tempo"][5], df_seq["Tempo"][7]]
 
 
 x = np.arange(len(labels))  # La posizione delle etichette
-width = 0.35  # La larghezza delle barre
+width = 0.3  # La larghezza delle barre
 
 fig, ax = plt.subplots()
 
 
-bars1 = ax.bar(x - width/2, distr, width, label='Distribuito')
-bars2 = ax.bar(x + width/2, seq, width, label='Sequenziale')
+bars1 = ax.bar(x - width, df_15["Tempo"], width, label='15 Mappers')
+bars2 = ax.bar(x, df_30["Tempo"], width, label='30 Mappers')
+bars3 = ax.bar(x + width, df_60["Tempo"], width, label='60 Mappers')
 
 
 ax.set_xlabel('Numero di Punti')
 ax.set_ylabel('Tempo')
-ax.set_title('Confronto tra esecuzione sequenziale e distribuita')
+ax.set_title('Confronto tra numero di mappers')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
@@ -51,8 +46,9 @@ def add_labels(bars):
                     textcoords="offset points",
                     ha='center', va='bottom')
 
-add_labels(bars1)
-add_labels(bars2)
+#add_labels(bars1)
+#add_labels(bars2)
+#add_labels(bars3)
 
 
 plt.show()
